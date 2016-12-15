af43d887f4b61189903112bfda37268827186add
Jeremie Dimino <jdimino@janestreet.com>
2016-12-15 11:20:46 +0000

    114.20+69

--- install.ml
+++
@@ -1,9 +0,0 @@
-#use "topfind";;
-#require "js-build-tools.oasis2opam_install";;
-
-open Oasis2opam_install;;
-
-generate ~package:"patience_diff"
-  [ oasis_lib "patience_diff_lib"
-  ; file "META" ~section:"lib"
-  ]
